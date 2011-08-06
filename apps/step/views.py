8bf22cb69a0ac1fa2eac5c95ef296e535bde8575
Marcin Swierczynski <marcin@swierczynski.net>
2011-08-06 17:35:59 +0200

    Simple authentication

--- apps/step/views.py
+++ apps/step/views.py
@@ -1,7 +1,12 @@
 # Create your views here.
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from step.models import Activity
 
+@login_required
 def list(request):
     activity = Activity.objects.all()[0]
-    return render_to_response('step/list.html', {'activity':activity})
+    return render_to_response('step/list.html',
+                              {'activity': activity},
+                              context_instance=RequestContext(request))
--- urls.py
+++ urls.py
@@ -15,5 +15,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+
     (r'^step/', include('step.urls'))
 )
